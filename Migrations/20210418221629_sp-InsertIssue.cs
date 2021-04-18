1e188d1803cd6539919e27a0630e44d4257af666
Andrew Kenworthy <akenworthy87@gmail.com>
2021-04-18 23:42:12 +0100

    Added InsertIssue stored proceedure

---
+++ Migrations/20210418221629_sp-InsertIssue.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCCRUD.Migrations
+{
+    public partial class spInsertIssue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE PROCEDURE [dbo].[InsertIssue]
+                        @Name nvarchar(50)
+                        ,@Location nvarchar(50)
+                        ,@SeverityId int
+                        ,@Description nvarchar(512)
+                        ,@Notes nvarchar(512)
+                        ,@Resolution nvarchar(512)
+                        ,@Resolved bit
+                    AS
+                    BEGIN
+                        DECLARE @SubmittedDate datetime2(7)
+								,@ResolvedDate datetime2(7)
+                        SET NOCOUNT ON;
+                        SET @SubmittedDate = GETDATE();
+                        IF (@Resolved = 1) SET @ResolvedDate = GETDATE();
+	
+
+                        INSERT INTO [dbo].[Issue]
+                                   ([Name]
+                                   ,[Location]
+                                   ,[SeverityId]
+                                   ,[SubmittedDate]
+                                   ,[ResolvedDate]
+                                   ,[Description]
+                                   ,[Notes]
+                                   ,[Resolution]
+                                   ,[Resolved])
+                             VALUES
+                                   (@Name
+			                        ,@Location
+			                        ,@SeverityId
+			                        ,@SubmittedDate
+			                        ,@ResolvedDate
+			                        ,@Description
+			                        ,@Notes
+			                        ,@Resolution
+			                        ,@Resolved)
+                    END";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
