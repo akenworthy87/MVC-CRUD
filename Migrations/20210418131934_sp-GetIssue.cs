fb3b4c00d7e0f29b853ed7857f7ff4b8ba5ca3d1
Andrew Kenworthy <akenworthy87@gmail.com>
2021-04-18 14:37:59 +0100

    Added GetIssue and GetSeverity SPs

---
+++ Migrations/20210418131934_sp-GetIssue.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCCRUD.Migrations
+{
+    public partial class spGetIssue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE PROCEDURE [dbo].[GetIssue]
+                        @Id int
+                    AS
+                    BEGIN
+                        SET NOCOUNT ON;
+                        SELECT [Id]
+                              ,[Name]
+                              ,[Location]
+                              ,[SeverityId]
+                              ,[SubmittedDate]
+                              ,[ResolvedDate]
+                              ,[Description]
+                              ,[Notes]
+                              ,[Resolution]
+                              ,[Resolved]
+                        FROM [MVC_CRUDContext-1].[dbo].[Issue]
+		                WHERE [ID] = @Id
+                    END";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Migrations/20210418133250_sp-GetSeverity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCCRUD.Migrations
+{
+    public partial class spGetSeverity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE PROCEDURE [dbo].[GetSeverity]
+                        @Id int
+                    AS
+                    BEGIN
+                        SET NOCOUNT ON;
+                        SELECT [Id]
+                              ,[SeverityName]
+                        FROM [MVC_CRUDContext-1].[dbo].[Severity]
+		                WHERE [ID] = @Id
+                    END";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
