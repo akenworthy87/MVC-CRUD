36ab8db2b4748309a749b8afdc3ef40d64b0cee5
Andrew Kenworthy <akenworthy87@gmail.com>
2021-04-19 00:24:32 +0100

    Added DeletedIssue sp

---
+++ Migrations/20210418231739_sp-DeleteIssue.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCCRUD.Migrations
+{
+    public partial class spDeleteIssue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE PROCEDURE [dbo].[DeleteIssue]
+                        @Id int
+                    AS
+                    BEGIN
+                        DELETE FROM [dbo].[Issue]
+                            WHERE [Id] = @Id
+                    END";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
