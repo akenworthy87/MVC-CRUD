0881a5947ad5cdd153b4de0f561429f5adb95c17
Andrew Kenworthy <akenworthy87@gmail.com>
2021-04-18 13:59:53 +0100

    Added migration for GetAllIssues stored proceedure

---
+++ Migrations/20210418124914_sp-GetAllIssues.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCCRUD.Migrations
+{
+    public partial class spGetAllIssues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"CREATE PROCEDURE [dbo].[GetAllIssues]
+                    AS
+                    BEGIN
+                        SET NOCOUNT ON;
+                        SELECT [Id]
+                              ,[Name]
+                              ,[Location]
+                              ,[SeverityId]
+                              ,[SubmittedDate]
+                              ,[ResolvedDate]
+                              ,[Description]
+                              ,[Notes]
+                              ,[Resolution]
+                              ,[Resolved]
+                          FROM [MVC_CRUDContext-1].[dbo].[Issue]
+                    END";
+
+                migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
