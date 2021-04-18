eb9a1cdd7224f4ea8de21ec25790e89408b469bb
Andrew Kenworthy <akenworthy87@gmail.com>
2021-04-18 20:25:22 +0100

    Made Read Controller and View Corrected Project name (changed - to _)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_CRUD.Data;
+using MVC_CRUD.Models;
 
 namespace MVC_CRUD.Controllers
 {
     public class HomeController : Controller
-{
-    public IActionResult Index()
     {
-        return View();
+        private readonly MVC_CRUDContext _context;
+
+        public HomeController(MVC_CRUDContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var issues = _context.Issue.FromSqlRaw("EXEC GetAllIssues").ToList();
+            var severities = _context.Severity.FromSqlRaw("EXEC GetAllSeverities").ToList();
+
+            ViewBag.SeverityID = new SelectList(severities, "Id", "SeverityName");
+
+            return View(issues);
+        }
     }
-}
 }
